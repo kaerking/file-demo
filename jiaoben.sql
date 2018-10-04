4146fb7e5c28334d19595731cfebb636b373f1ed
kaerking <43069091+kaerking@users.noreply.github.com>
2018-10-04 20:13:34 +0800

    Update jiaoben.sql

--- jiaoben.sql
+++ jiaoben.sql
@@ -1,1 +1,7 @@
+CREATE TABLE `t_file` (
+  `id` varchar(200) DEFAULT NULL COMMENT '主键',
+  `name` varchar(100) DEFAULT NULL COMMENT '名称',
+  `size` bigint(20) DEFAULT NULL COMMENT '大小',
+  `path` varchar(200) DEFAULT NULL COMMENT '路径'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
